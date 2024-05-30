840d497a14f0f1c39e32fcc0fd514cec0d9550fc
Aleksandr Soloshenko <capcom@soft-c.ru>
2024-05-30 13:25:51 +0700

    [008-nestjs-db/part-1] install mongoose and init connection

--- 008-nestjs-db/src/app.module.ts
+++ 008-nestjs-db/src/app.module.ts
@@ -2,10 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BooksModule } from './books/books.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BooksModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URL || 'mongodb://localhost:27017/library'),
+    BooksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
