286e4f730b460fb1a89357fa95fe96fcf4f98c1c
Aleksandr Soloshenko <capcom@soft-c.ru>
2024-06-06 14:07:21 +0700

    [009-rxjs/part-01] add GitHub request

--- 009-rxjs/src/index.ts
+++ 009-rxjs/src/index.ts
@@ -1,8 +1,17 @@
-import {of, from, timer, range} from 'rxjs';
+import { of, from } from 'rxjs';
+import { map, mergeMap } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
 
-const o = range(0, 10)
+const url = 'https://api.github.com/search/repositories?q=rxjs';
+const o$ = ajax(url)
+  .pipe(
+    map((response) => response.response),
+    map((json) => json.items),
+    mergeMap((items) => of(...items)),
+    map((item) => item.full_name),
+  );
 
-o.subscribe({
+o$.subscribe({
   next: (value: any) => console.log('Next:', value),
   complete: () => console.log('Complete!'),
   error: (error) => console.log('Error!', error)
