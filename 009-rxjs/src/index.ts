8673dc6b2a84900e105489a8da07551c38b4b7a0
Aleksandr Soloshenko <capcom@soft-c.ru>
2024-06-06 14:13:08 +0700

    [009-rxjs/part-02] add GitLab request

--- 009-rxjs/src/index.ts
+++ 009-rxjs/src/index.ts
@@ -2,17 +2,39 @@ import { of, from } from 'rxjs';
 import { map, mergeMap } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 
-const url = 'https://api.github.com/search/repositories?q=rxjs';
-const o$ = ajax(url)
-  .pipe(
-    map((response) => response.response),
-    map((json) => json.items),
-    mergeMap((items) => of(...items)),
-    map((item) => item.full_name),
-  );
+function github() {
+  const url = 'https://api.github.com/search/repositories?q=rxjs';
+  const o$ = ajax(url)
+    .pipe(
+      map((response) => response.response),
+      map((json) => json.items),
+      mergeMap((items) => of(...items)),
+      map((item) => item.full_name),
+    );
 
-o$.subscribe({
-  next: (value: any) => console.log('Next:', value),
-  complete: () => console.log('Complete!'),
-  error: (error) => console.log('Error!', error)
-})
+  return o$;
+}
+
+function gitlab() {
+  const url = 'https://gitlab.com/api/v4/projects?search=rxjs';
+  const o$ = ajax.getJSON(url)
+    .pipe(
+      mergeMap((json: any) => of(...json)),
+      map((item: any) => item.path_with_namespace),
+    );
+
+  return o$;
+}
+
+
+github().subscribe({
+  next: (value: any) => console.log('GitHub:', value),
+  complete: () => console.log('GitHub Complete!'),
+  error: (error) => console.log('GitHub Error!', error)
+});
+
+gitlab().subscribe({
+  next: (value: any) => console.log('GitLab:', value),
+  complete: () => console.log('GitLab Complete!'),
+  error: (error) => console.log('GitLab Error!', error)
+});
