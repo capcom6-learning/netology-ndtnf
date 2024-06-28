cbac4f1a03c5a1883717d1c94150b47144999d55
Aleksandr Soloshenko <capcom2me@gmail.com>
2024-06-28 20:47:09 +0700

    [015-Yandex-ID] initial commit

---
+++ 015-Yandex-ID/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { BooksModule } from './books/books.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
+import { RouterModule } from '@nestjs/core';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URL || 'mongodb://localhost:27017/library'),
+    BooksModule,
+    AuthModule,
+    RouterModule.register([
+      {
+        path: 'api',
+        module: AuthModule,
+      },
+    ])
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule { }
---
+++ 015-Yandex-ID/src/auth/auth.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { User, UserModel } from './auth.models';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: User.name, schema: UserModel }]),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: '2 days',
+        }
+      }),
+    })
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy],
+})
+export class AuthModule { }
